0832368c82b889d420f11a35b06f8e9b021c54a3
Dong <dong-18@live.de>
2019-05-29 18:27:53 +0200

    create head

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,1 +1,32 @@
-export default () => <div>Hello World</div>
+import React, { Component } from 'react'
+import Head from 'next/head'
+
+import '../scss/home.scss'
+
+class Home extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+                <Head>
+                    <title>My page title</title>
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1.0, width=device-width"
+                        key="viewport"
+                    />
+                </Head>
+                <Head>
+                    <meta
+                        name="viewport"
+                        content="initial-scale=1.2, width=device-width"
+                        key="viewport"
+                    />
+                </Head>
+                <p>Hello world!</p>
+            </div>
+        );
+    }
+}
+
+export default Home;
