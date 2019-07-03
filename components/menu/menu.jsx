6cb6ce29b96ac1aaaf922456bd97b45ab806b2e2
Dong <dong-18@live.de>
2019-07-03 16:55:39 +0200

    build menu

--- components/menu/menu.jsx
+++ components/menu/menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Container, Row, Col, Button } from 'reactstrap'
 import Link from 'next/link'
 
 class Menu extends React.Component {
@@ -7,19 +8,49 @@ class Menu extends React.Component {
         items: PropTypes.array
     }
 
-    renderItems(items) {
+    renderLogo = () => {
         return (
-            <p>{items.name}</p>
+            <Col className='home-logo' md='2'>
+                LOGO
+            </Col>
+        )
+    }
+
+    renderMenu = items => {
+        return (
+            <Col className='menu'>
+                <nav className='main-nav float-right'>
+                    <ul>
+                        {items && items.map(item => this.renderItems(item))}
+                    </ul>
+                </nav>
+            </Col>
+        )
+    }
+
+    renderItems = items => {
+        return (
+            <li>
+                <Link href={items.link}>
+                    <a>
+                        {items.name}
+                    </a>
+                </Link>
+            </li>
         )
     }
 
     render() {
-        console.log(this.props);
         const { items } = this.props
         return (
-            <div className='menu'>
-                {items && items.map(item => this.renderItems(item))}
-            </div>
+            <header className='header fixed-top'>
+                <Container>
+                    <Row>
+                        {this.renderLogo()}
+                        {this.renderMenu(items)}
+                    </Row>
+                </Container>
+            </header>
         )
     }
 }
