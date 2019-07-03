87353198b033ccfb0da34d9f7091820fcc46b73f
Dong <dong-18@live.de>
2019-07-03 16:56:00 +0200

    content changes

---
+++ lib/dummydata/widgets.jsx
@@ -0,0 +1,23 @@
+export default 
+{
+    "id" : 2,
+    "name" : "widgets",
+    "items" : [
+        {
+            "name": "intro", 
+            "content": "some text",
+        },
+        {
+            "name": "about us", 
+            "content": "some text",
+        },
+        {
+            "name": "services", 
+            "content": "some text",
+        },
+        {
+            "name": "portfolio", 
+            "content": "some text",
+        }
+    ]
+}
