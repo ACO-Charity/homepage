db8d9ccda670f7c04e1bc5a0124e30e0617e4914
Dong <dong-18@live.de>
2019-07-09 14:26:50 +0200

    fix menuReducer spacing

--- reducers/menuReducer.jsx
+++ reducers/menuReducer.jsx
@@ -2,21 +2,22 @@ import { LOAD_MENU } from '../constants/actionTypes';
 import update from 'immutability-helper';
 
 const initialState = {
-	pending : false,
-	error   : null,
-	menus   : {
-		main    : { items: [] }
+	pending: false,
+	error: null,
+	menus: {
+		main: { items: [] }
 	}
 };
 
-export default function reducer (state = initialState, action) {
+export default function reducer(state = initialState, action) {
+
 	switch (action.type) {
 		case LOAD_MENU + '_PENDING': {
 			return update(state,
 				{
 					$merge: {
-						pending : true,
-						error   : null
+						pending: true,
+						error: null
 					},
 					menus: {
 						[action.meta.menu]: {
@@ -29,10 +30,10 @@ export default function reducer (state = initialState, action) {
 			return update(state,
 				{
 					$merge: {
-						pending : false,
-						error   : {
-							status  : action.payload.response.status,
-							message : action.payload.message
+						pending: false,
+						error: {
+							status: action.payload.response.status,
+							message: action.payload.message
 						}
 					},
 					menus: {
@@ -46,15 +47,15 @@ export default function reducer (state = initialState, action) {
 			return update(state,
 				{
 					$merge: {
-						pending : false,
-						error   : null
+						pending: false,
+						error: null
 					},
 					menus: {
 						[action.meta.menu]: {
-              $set: {
-                items: action.payload.data.items,
-                id: action.meta.id
-              }
+							$set: {
+								items: action.payload.data.items,
+								id: action.meta.id
+							}
 						}
 					}
 				});
