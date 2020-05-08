2281a419021709db0ee9bdd80b677d6fe7bcd050
Henry Pham <h.pham@pripares.com>
2020-05-08 13:22:05 +0200

    #hp08May2020 cleaning page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import logo from './img/Aco_Logo_Blau.svg';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} alt="logo"/>
-            </header>
+        <div className="aco-web">
         </div>
     );
 }
