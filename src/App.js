ea84f04d7a8185f5d2693b41c404b487110dbd72
Pham Minh Hoang Henry <minhhoangpham@live.de>
2020-05-09 13:21:46 +0200

    splitting content to landingpage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import labels_de from './i18n/de';
 import Footer from './components/Footer';
 import languages from "./enums/languages";
 import labels_en from "./i18n/en";
+import LandingPage from "./components/LandingPage";
 
 function App() {
 
@@ -37,111 +38,7 @@ function App() {
         <div className="aco-web">
             <Header scrollPosY={scrollPosY} selectedLanguage={selectedLanguage} setSelectedLanguage={setSelectedLanguage}/>
             <div className="content">
-                <section id={pages.ABOUT_US}>
-                    <div className="container jumbotron">
-                        <h1>{label()[pages.ABOUT_US]}</h1>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                    </div>
-                </section>
-                <section id={pages.NEWS}>
-                    <div className="container jumbotron">
-                        <h1>{label()[pages.NEWS]}</h1>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                    </div>
-                </section>
-                <section id={pages.PROJECTS}>
-                    <div className="container jumbotron">
-                        <h1>{label()[pages.PROJECTS]}</h1>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                    </div>
-                </section>
-                <section id={pages.DONATION}>
-                    <div className="container jumbotron">
-                        <h1>{label()[pages.DONATION]}</h1>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                    </div>
-                </section>
-                <section id={pages.JOIN_US}>
-                    <div className="container jumbotron">
-                        <h1>{label()[pages.JOIN_US]}</h1>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                        <p> aksöldksaldsaöld</p>
-                        <p>asdkölsakdölsakd</p>
-                        <p>sadölsak</p>
-                    </div>
-                </section>
+                <LandingPage label={label()} />
             </div>
             <Footer selectedLanguage={selectedLanguage}/>
         </div>
---
+++ src/components/LandingPage.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import pages from "../enums/pages";
+
+const LandingPage = props => {
+    return (
+        <>
+            <section id={pages.ABOUT_US}>
+                <div className="container jumbotron">
+                    <h1>{props.label[pages.ABOUT_US]}</h1>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                </div>
+            </section>
+            <section id={pages.NEWS}>
+                <div className="container jumbotron">
+                    <h1>{props.label[pages.NEWS]}</h1>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                </div>
+            </section>
+            <section id={pages.PROJECTS}>
+                <div className="container jumbotron">
+                    <h1>{props.label[pages.PROJECTS]}</h1>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                </div>
+            </section>
+            <section id={pages.DONATION}>
+                <div className="container jumbotron">
+                    <h1>{props.label[pages.DONATION]}</h1>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                </div>
+            </section>
+            <section id={pages.JOIN_US}>
+                <div className="container jumbotron">
+                    <h1>{props.label[pages.JOIN_US]}</h1>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                    <p> aksöldksaldsaöld</p>
+                    <p>asdkölsakdölsakd</p>
+                    <p>sadölsak</p>
+                </div>
+            </section>
+        </>
+    );
+};
+
+
+export default LandingPage;
