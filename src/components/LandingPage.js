965c77434dbd0a595dab281e23271434d2885b13
Pham Minh Hoang Henry <minhhoangpham@live.de>
2020-05-29 13:22:37 +0200

    adding webp support

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import pages from "../enums/pages";
 import BasicSection from "./common/BasicSection";
-import newsSectionImage from '../img/section_news.jpg';
-import projectsSectionImage from '../img/section_projects.jpg';
-import donateSectionImage from '../img/section_donate.jpg';
-import joinSectionImage from '../img/section_join.jpg';
 import ACOFacts from "./ACOFacts";
 import AboutUs from './sections/AboutUs';
+import {isWebpSupported} from 'react-image-webp/dist/utils';
 
 const LandingPage = props => {
     return (
@@ -18,7 +15,7 @@ const LandingPage = props => {
             <ACOFacts label={props.label}/>
             <BasicSection sectionId={pages.NEWS}
                           setCurrentSection={props.setCurrentSection}
-                          sectionImage={newsSectionImage}
+                          sectionImage={isWebpSupported()? require('../img/section_news.webp') : require('../img/section_news.jpg')}
                           sectionTitle={props.label[pages.NEWS]}>
                 <p> aksöldksaldsaöld</p>
                 <p>asdkölsakdölsakd</p>
@@ -39,7 +36,7 @@ const LandingPage = props => {
             </BasicSection>
             <BasicSection sectionId={pages.PROJECTS}
                           setCurrentSection={props.setCurrentSection}
-                          sectionImage={projectsSectionImage}
+                          sectionImage={isWebpSupported()? require('../img/section_projects.webp') : require('../img/section_projects.jpg')}
                           sectionTitle={props.label[pages.PROJECTS]}>
                 <p> aksöldksaldsaöld</p>
                 <p>asdkölsakdölsakd</p>
@@ -60,7 +57,7 @@ const LandingPage = props => {
             </BasicSection>
             <BasicSection sectionId={pages.DONATION}
                           setCurrentSection={props.setCurrentSection}
-                          sectionImage={donateSectionImage}
+                          sectionImage={isWebpSupported()? require('../img/section_donate.webp') : require('../img/section_donate.jpg')}
                           sectionTitle={props.label[pages.DONATION]}>
                 <p> aksöldksaldsaöld</p>
                 <p>asdkölsakdölsakd</p>
@@ -81,7 +78,7 @@ const LandingPage = props => {
             </BasicSection>
             <BasicSection sectionId={pages.JOIN_US}
                           setCurrentSection={props.setCurrentSection}
-                          sectionImage={joinSectionImage}
+                          sectionImage={isWebpSupported()? require('../img/section_join.webp') : require('../img/section_join.jpg')}
                           sectionTitle={props.label[pages.JOIN_US]}>
                 <p> aksöldksaldsaöld</p>
                 <p>asdkölsakdölsakd</p>
--- src/components/sections/AboutUs.js
+++ src/components/sections/AboutUs.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import pages from '../../enums/pages';
-import aboutSectionImage from '../../img/section_about.jpg';
 import BasicSection from '../common/BasicSection';
+import {isWebpSupported} from "react-image-webp/dist/utils";
 
 const AboutUs = props => {
     return (
         <BasicSection
             setCurrentSection={props.setCurrentSection}
             sectionId={pages.ABOUT_US}
-            sectionImage={aboutSectionImage}
+            sectionImage={isWebpSupported()? require('../../img/section_about.webp') : require('../../img/section_about.jpg')}
             sectionTitle={props.label[pages.ABOUT_US]}>
             <p>{props.label.ABOUT_SECTION_TEXT}</p>
         </BasicSection>
