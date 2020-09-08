3b05173118fbbfac3807846d58b9a12587992e9d
Henry Pham <h.pham@pripares.com>
2020-09-08 16:22:33 +0200

    #hp08Sep2020 project section is own file now

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,37 +5,17 @@ import pages from "../enums/pages";
 import AboutUs from './sections/AboutUs';
 import BasicSection from "./common/BasicSection";
 import {isWebpSupported} from 'react-image-webp/dist/utils';
-import languages from '../enums/languages';
+import ProjectsSection from './ProjectsSection';
 
 const LandingPage = props => {
     return (
         <>
-            <AboutUs
-                setCurrentSection={props.setCurrentSection}
-                label={props.label}
+            <AboutUs setCurrentSection={props.setCurrentSection}
+                     label={props.label}
             />
             <ACOFacts label={props.label}/>
-            <BasicSection sectionId={pages.PROJECTS}
-                          setCurrentSection={props.setCurrentSection}
-                          sectionImage={isWebpSupported() ? require('../img/section_projects.webp') : require('../img/section_projects.jpg')}
-                          sectionTitle={props.label[pages.PROJECTS]}>
-                <p>{props.label.PROJECT_SECTION_TEXT}</p>
-
-                <h3 className="mt-5">{props.label.PROJECT_BIEN_HOA_TITLE}</h3>
-                <p>{props.label.PROJECT_BIEN_HOA_TEXT_1}</p>
-                <p>{props.label.PROJECT_BIEN_HOA_TEXT_2}</p>
-
-                <h3 className="mt-5">{props.label.PROJECT_HLT_TITLE}</h3>
-                <p>{props.label.PROJECT_HLT_TEXT_1}</p>
-
-                <h3 className="mt-5">{props.label.PROJECT_VS_TITLE}</h3>
-                {
-                    props.selectedLanguage === languages.DE &&
-                    <p className="text-center mt-5 mb-5"><i>{props.label.PROJECT_VS_QOUTE}</i></p>
-                }
-                <p>{props.label.PROJECT_VS_TEXT_1}</p>
-                <p>{props.label.PROJECT_VS_TEXT_2}</p>
-            </BasicSection>
+            <ProjectsSection setCurrentSection={props.setCurrentSection}
+                             label={props.label}/>
             <BasicSection sectionId={pages.DONATION}
                           setCurrentSection={props.setCurrentSection}
                           sectionImage={isWebpSupported() ? require('../img/section_donate.webp') : require('../img/section_donate.jpg')}
---
+++ src/components/ProjectsSection.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import pages from '../enums/pages';
+import {isWebpSupported} from "react-image-webp/dist/utils";
+import languages from '../enums/languages';
+import BasicSection from './common/BasicSection';
+
+const ProjectsSection = props => {
+    return (
+        <BasicSection sectionId={pages.PROJECTS}
+                      setCurrentSection={props.setCurrentSection}
+                      sectionImage={isWebpSupported() ? require('../img/section_projects.webp') : require('../img/section_projects.jpg')}
+                      sectionTitle={props.label[pages.PROJECTS]}>
+            <p>{props.label.PROJECT_SECTION_TEXT}</p>
+
+            <h3 className="mt-5">{props.label.PROJECT_BIEN_HOA_TITLE}</h3>
+            <p>{props.label.PROJECT_BIEN_HOA_TEXT_1}</p>
+            <p>{props.label.PROJECT_BIEN_HOA_TEXT_2}</p>
+
+            <h3 className="mt-5">{props.label.PROJECT_HLT_TITLE}</h3>
+            <p>{props.label.PROJECT_HLT_TEXT_1}</p>
+
+            <h3 className="mt-5">{props.label.PROJECT_VS_TITLE}</h3>
+            {
+                props.selectedLanguage === languages.DE &&
+                <p className="text-center mt-5 mb-5"><i>{props.label.PROJECT_VS_QOUTE}</i></p>
+            }
+            <p>{props.label.PROJECT_VS_TEXT_1}</p>
+            <p>{props.label.PROJECT_VS_TEXT_2}</p>
+        </BasicSection>
+    );
+};
+
+export default ProjectsSection;
