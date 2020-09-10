d972260977b58fb149494ec420cd7804f1299af8
Henry Pham <h.pham@pripares.com>
2020-09-10 17:16:27 +0200

    #hp10Sep2020 HOM-32 update donation number

--- src/components/FactsNumber.js
+++ src/components/FactsNumber.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Spring } from "react-spring/renderprops";
+import {Spring} from "react-spring/renderprops";
 
 const FactsNumber = props => {
 
-  return (
-    <div className="fact-num">
-      <Spring
-        delay={400}
-        config={{ duration: 800 }}
-        unit={props.unit}
-        from={{ number: 0 }}
-        to={{ number: props.num }}>
-        {springProps => <p className="fact-num-val">{Math.floor(springProps.number)} {props.unit}</p>}
-      </Spring>
-      <p className="fact-num-text">{props.text}</p>
-    </div>
-  );
+    return (
+        <div className="fact-num">
+            <Spring
+                delay={400}
+                config={{duration: 800}}
+                unit={props.unit}
+                from={{number: 0}}
+                to={{number: props.num}}>
+                {springProps => <p className="fact-num-val">{springProps.number.toLocaleString()} {props.unit}</p>}
+            </Spring>
+            <p className="fact-num-text">{props.text}</p>
+        </div>
+    );
 };
 
 export default FactsNumber;
