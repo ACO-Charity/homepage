87f99c2f951d4096256d910c11e3cb2adbb50b45
Henry Pham <h.pham@pripares.com>
2020-09-15 09:55:25 +0200

    #hp15Sep2020 HOM-34 add postal address into imprint

--- src/i18n/imprint-text.js
+++ src/i18n/imprint-text.js
@@ -1,72 +1,74 @@
 const imprintText = {
-	DE: {
-		HQ: 'Vereinssitz',
-		REGISTRY: 'Registereintrag',
-		COURT: 'Registergericht: Bayern Amtsgericht München',
-		REG_NUMBER: 'Registernummer: VR 202825',
-		DISCLAIMER: 'Haftungsausschluss',
+    DE: {
+        POSTAL: 'Postanschrift',
+        HQ: 'Vereinssitz',
+        REGISTRY: 'Registereintrag',
+        COURT: 'Registergericht: Bayern Amtsgericht München',
+        REG_NUMBER: 'Registernummer: VR 202825',
+        DISCLAIMER: 'Haftungsausschluss',
 
-		LIABILITY_CONTENT_TITLE: 'Haftung für Inhalte',
-		LIABILITY_CONTENT_TEXT: 'Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. ' +
-			'Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. ' +
-			'Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. ' +
-			'Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen ' +
-			'oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. ' +
-			'Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. ' +
-			'Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. ' +
-			'Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.',
+        LIABILITY_CONTENT_TITLE: 'Haftung für Inhalte',
+        LIABILITY_CONTENT_TEXT: 'Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. ' +
+            'Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. ' +
+            'Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. ' +
+            'Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen ' +
+            'oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. ' +
+            'Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. ' +
+            'Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. ' +
+            'Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.',
 
-		LIABILITY_LINKS_TITLE: 'Haftung für Links',
-		LIABILITY_LINKS_TEXT: 'Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. ' +
-			'Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. ' +
-			'Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. ' +
-			'Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. ' +
-			'Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.' +
-			'Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. ' +
-			'Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.',
+        LIABILITY_LINKS_TITLE: 'Haftung für Links',
+        LIABILITY_LINKS_TEXT: 'Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. ' +
+            'Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. ' +
+            'Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. ' +
+            'Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. ' +
+            'Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.' +
+            'Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. ' +
+            'Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.',
 
-		COPYRIGHT_TITLE: 'Urheberrecht',
-		COPYRIGHT_TEXT: 'Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. ' +
-			'Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung ' +
-			'des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.' +
-			'Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. ' +
-			'Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, ' +
-			'bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.',
-	},
+        COPYRIGHT_TITLE: 'Urheberrecht',
+        COPYRIGHT_TEXT: 'Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. ' +
+            'Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung ' +
+            'des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.' +
+            'Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. ' +
+            'Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, ' +
+            'bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.'
+    },
 
-	EN: {
-		HQ: 'Registered office',
-		REGISTRY: 'Registry entry',
-		COURT: 'Register Court: Bavaria Local Court Munich',
-		REG_NUMBER: 'Registration number: VR 202825',
-		DISCLAIMER: 'Disclaimer',
+    EN: {
+        POSTAL: 'Postal Address',
+        HQ: 'Registered office',
+        REGISTRY: 'Registry entry',
+        COURT: 'Register Court: Bavaria Local Court Munich',
+        REG_NUMBER: 'Registration number: VR 202825',
+        DISCLAIMER: 'Disclaimer',
 
-		LIABILITY_CONTENT_TITLE: 'Liability for contents',
-		LIABILITY_CONTENT_TEXT: 'The contents of our pages were created with the greatest care. ' +
-			'However, we cannot guarantee that the contents are correct, complete and up-to-date. ' +
-			'As a service provider, we are responsible for our own content on these pages in accordance with § 7.1 TMG (German Telemedia Act) and general laws. ' +
-			'According to §§ 8 to 10 TMG we are not obliged to monitor transmitted or stored information from third parties or ' +
-			'to investigate circumstances that indicate illegal activity. ' +
-			'Obligations to remove or block the use of information according to general laws remain unaffected by this. ' +
-			'However, liability in this respect is only possible from the time of knowledge of a concrete infringement. ' +
-			'If we become aware of any such legal infringements, we will remove the content in question immediately.',
+        LIABILITY_CONTENT_TITLE: 'Liability for contents',
+        LIABILITY_CONTENT_TEXT: 'The contents of our pages were created with the greatest care. ' +
+            'However, we cannot guarantee that the contents are correct, complete and up-to-date. ' +
+            'As a service provider, we are responsible for our own content on these pages in accordance with § 7.1 TMG (German Telemedia Act) and general laws. ' +
+            'According to §§ 8 to 10 TMG we are not obliged to monitor transmitted or stored information from third parties or ' +
+            'to investigate circumstances that indicate illegal activity. ' +
+            'Obligations to remove or block the use of information according to general laws remain unaffected by this. ' +
+            'However, liability in this respect is only possible from the time of knowledge of a concrete infringement. ' +
+            'If we become aware of any such legal infringements, we will remove the content in question immediately.',
 
-		LIABILITY_LINKS_TITLE: 'Liability for links',
-		LIABILITY_LINKS_TEXT: 'Our offer contains links to external websites of third parties, on whose contents we have no influence. ' +
-			'Therefore we cannot assume any liability for these external contents. ' +
-			'The respective provider or operator of the sites is always responsible for the contents of the linked sites. ' +
-			'The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not identified at the time of linking.' +
-			'However, a permanent control of the contents of the linked pages is not reasonable without concrete evidence of a violation of the law. ' +
-			'If we become aware of any infringements, we will remove such links immediately.',
+        LIABILITY_LINKS_TITLE: 'Liability for links',
+        LIABILITY_LINKS_TEXT: 'Our offer contains links to external websites of third parties, on whose contents we have no influence. ' +
+            'Therefore we cannot assume any liability for these external contents. ' +
+            'The respective provider or operator of the sites is always responsible for the contents of the linked sites. ' +
+            'The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not identified at the time of linking.' +
+            'However, a permanent control of the contents of the linked pages is not reasonable without concrete evidence of a violation of the law. ' +
+            'If we become aware of any infringements, we will remove such links immediately.',
 
-		COPYRIGHT_TITLE: 'Copyright',
-		COPYRIGHT_TEXT: 'The contents and works on these pages created by the site operators are subject to German copyright law. ' +
-			'The reproduction, editing, distribution and any kind of use outside the limits of copyright law require the written consent of the respective author or creator. ' +
-			'Downloads and copies of these pages are only permitted for private, non-commercial use.' +
-			'Insofar as the content on this site was not created by the operator, the copyrights of third parties are observed. ' +
-			'In particular, third-party content is identified as such. Should you nevertheless become aware of a copyright infringement, please inform us accordingly. ' +
-			'If we become aware of any infringements, we will remove such contents immediately.',
-	}
-}
+        COPYRIGHT_TITLE: 'Copyright',
+        COPYRIGHT_TEXT: 'The contents and works on these pages created by the site operators are subject to German copyright law. ' +
+            'The reproduction, editing, distribution and any kind of use outside the limits of copyright law require the written consent of the respective author or creator. ' +
+            'Downloads and copies of these pages are only permitted for private, non-commercial use.' +
+            'Insofar as the content on this site was not created by the operator, the copyrights of third parties are observed. ' +
+            'In particular, third-party content is identified as such. Should you nevertheless become aware of a copyright infringement, please inform us accordingly. ' +
+            'If we become aware of any infringements, we will remove such contents immediately.'
+    }
+};
 
 export default imprintText;
